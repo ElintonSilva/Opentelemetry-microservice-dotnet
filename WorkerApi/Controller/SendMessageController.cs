ced8adfcea29144d877cea69dba285349a9ac07a
Elinton <elintones@gmail.com>
2025-01-19 22:57:12 -0300

    Adicionar arquivos de projeto.

---
+++ WorkerApi/Controller/SendMessageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Utils.Messaging;
+
+namespace WorkerApi.Controller
+{
+    [ApiController]
+    [Route("/")]
+    public class SendMessageController : ControllerBase
+    {
+        private readonly ILogger<SendMessageController> logger;
+        private readonly MessageSender messageSender;
+
+        public SendMessageController(ILogger<SendMessageController> logger, MessageSender messageSender)
+        {
+            this.logger = logger;
+            this.messageSender = messageSender;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return this.messageSender.SendMessage();
+        }
+    }
+
+}
---
+++ WorkerApi/Startup.cs
@@ -0,0 +1,47 @@
+﻿using OpenTelemetry.Trace;
+using Utils.Messaging;
+
+namespace WorkerApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSingleton<MessageSender>();
+
+            services.AddOpenTelemetry()
+                .WithTracing(builder => builder
+                    .AddAspNetCoreInstrumentation()
+                    .AddSource(nameof(MessageSender))
+                    .AddZipkinExporter(b =>
+                    {
+                        var zipkinHostName = Environment.GetEnvironmentVariable("ZIPKIN_HOSTNAME") ?? "localhost";
+                        b.Endpoint = new Uri($"http://{zipkinHostName}:9411/api/v2/spans");
+                    }));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
